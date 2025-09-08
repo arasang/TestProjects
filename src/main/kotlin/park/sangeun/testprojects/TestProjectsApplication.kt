f5647f1817426aefb54dfd6c540cd375d452206d
Amber <separk@yanadoocorp.com>
2025-09-08 22:22:52 +0900

    git 누락 추가

---
+++ src/main/kotlin/park/sangeun/testprojects/TestProjectsApplication.kt
@@ -0,0 +1,11 @@
+package park.sangeun.testprojects
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TestProjectsApplication
+
+fun main(args: Array<String>) {
+    runApplication<TestProjectsApplication>(*args)
+}
---
+++ src/test/kotlin/park/sangeun/testprojects/TestProjectsApplicationTests.kt
@@ -0,0 +1,13 @@
+package park.sangeun.testprojects
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestProjectsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
